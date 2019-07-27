7cdc39b21dbba347335a86b9e770543e365b8c86
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-07-27 15:13:06 -0500

    Started work on conversion functions

---
+++ conversion.js
@@ -0,0 +1,9 @@
+function getAge() {
+    var age = document.getElementById("age");
+}
+
+function getHeight() {
+    var heightFeet = document.getElementById("heightFeet");
+    var heightInches = document.getElementById("heightInches");
+    
+}
