1b2bab1b5bf739ab49625cb0d5b1ae161ebe693c
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-08-01 13:02:58 -0500

    Added comments to conversion.js

--- conversion.js
+++ conversion.js
@@ -1,9 +1,16 @@
+//This JS file will convert the data entered by the users into the parameters
+//that will be used as part of the GET request
+//The parameters being: flex, curve, ageLevel, price
+
+//get age from user input and turn it into integer
 function getAge() {
     var age = document.getElementById("age");
     var ageInt = parseInt(age);
     return ageInt;
 }
 
+//get height from user input - turn the height in inches into integer and height in feet into integer
+//convert those to get total height in inches
 function getHeight() {
     var heightFeet = document.getElementById("heightFeet");
     var heightInches = document.getElementById("heightInches");
@@ -15,22 +22,26 @@ function getHeight() {
     return TotalHeight;
 }
 
+//get weight from user input - turn weight into integer
 function getWeight() {
     var weight = document.getElementById("weight");
     var weightInt = parseInt(weight);
     return weightInt;
 }
 
+//get position from user input
 function getPosition() {
     var position = document.getElementById("position");
     return position;
 }
 
+//get playing style from user input
 function getPlayingStyle() {
     var playingStyle = document.getElementById("playingStyle");
     return playingStyle;
 }
 
+//get maximum price from user input - turn price into integer
 function getMaxPrice() {
     var maxPrice = document.getElementById("maxPrice");
     var maxPriceInt = parseInt(maxPrice);
@@ -39,6 +50,7 @@ function getMaxPrice() {
 
 var ageLevel;
 
+//use user's weight and height to determine what age level of stick best fits them (Youth/Junior/Intermediate/Senior)
 function findAgeLevel(weightInt, TotalHeight) {
     if(weightInt < 120 && TotalHeight < 45) {
         ageLevel = "Youth";
@@ -92,52 +104,56 @@ function findAgeLevel(weightInt, TotalHeight) {
 
 var flex;
 
+//use user's age level (determined by last function), age, and position to determine what flex (stiffness) best fits their needs
 function findFlex(ageLevel, ageInt, position) {
     if(ageLevel == "Junior" && ageInt <= 10) {
         flex = 40;
     }
-    if(ageLevel == "Junior" && ageInt > 10) {
+    else if(ageLevel == "Junior" && ageInt > 10) {
         flex = 50;
     }
-    if(ageLevel == "Intermediate" && ageInt <= 12) {
+    else if(ageLevel == "Intermediate" && ageInt <= 12) {
         flex = 55;
     }
-    if(ageLevel == "Intermediate" && ageInt > 12 && ageInt <=15) {
+    else if(ageLevel == "Intermediate" && ageInt > 12 && ageInt <=15) {
         flex = 60;
     }
-    if(ageLevel == "Intermediate" && ageInt > 16) {
+    else if(ageLevel == "Intermediate" && ageInt > 16) {
         flex = 65;
     }
-    if(ageLevel == "Senior" && ageInt <= 18) {
+    else if(ageLevel == "Senior" && ageInt <= 18) {
         flex = 77;
     }
-    if(ageLevel == "Senior" && ageInt > 18 && ageInt <= 22) {
+    else if(ageLevel == "Senior" && ageInt > 18 && ageInt <= 22) {
         flex = 87;
     }
-    if(ageLevel == "Senior" && ageInt > 22 && ageInt <= 30) {
+    else if(ageLevel == "Senior" && ageInt > 22 && ageInt <= 30) {
         flex = 95;
     }
-    if(ageLevel == "Senior" && ageInt > 22 && ageInt <= 30 && position =="D") {
+    else if(ageLevel == "Senior" && ageInt > 22 && ageInt <= 30 && position =="D") {
         flex = 102;
     }
+    return flex;
 }
 
 var curve;
 
+//use user's position and playing style to determine which stick curve best fits their needs
 function findCurve(position, playingStyle) {
     if((position == "LW" || position == "C" || position == "RW") && playingStyle == "jackofalltrades") {
         curve = "P92"
     }
-    if((position == "LW" || position == "C" || position == "RW") && playingStyle == "playmaker") {
+    else if((position == "LW" || position == "C" || position == "RW") && playingStyle == "playmaker") {
         curve = "P88"
     }
-    if((position == "LW" || position == "C" || position == "RW") && playingStyle == "sniper") {
+    else if((position == "LW" || position == "C" || position == "RW") && playingStyle == "sniper") {
         curve = "P28"
     }
-    if(position == "D" && playingStyle == "slapshot"){
+    else if(position == "D" && playingStyle == "slapshot"){
         curve = "P02"
     }
-    if((position == "LW" || position == "C" || position == "RW") && playingStyle == "dangler") {
+    else if((position == "LW" || position == "C" || position == "RW") && playingStyle == "dangler") {
         curve = "P92"
     }
+    return curve;
 }
